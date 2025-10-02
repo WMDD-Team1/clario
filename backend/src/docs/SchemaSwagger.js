4761288d9b86b343ce6f34b5ab830d3d0c78ebb7
Bitna / Erin <64954355+bitna-dev@users.noreply.github.com>
2025-10-01 17:46:29 -0700

    refactor(login): return a user

--- backend/src/docs/SchemaSwagger.js
+++ backend/src/docs/SchemaSwagger.js
@@ -2,39 +2,6 @@
  * @swagger
  * components:
  *   schemas:
- *     Signup:
- *       type: object
- *       properties:
- *         email:
- *           type: string
- *           example: bitna@test.com
- *         name:
- *           type: string
- *           example: Bitna Lee
- *         userType:
- *           type: string
- *           enum: [Freelancer, Contractor]
- *           nullable: true
- *           example: null
- *         profileImage:
- *           type: string
- *           example: null
- *           nullable: true
- *         province:
- *           type: string
- *           example: null
- *           nullable: true
- *         currency:
- *           type: string
- *           example: null
- *           nullable: true
- *         onBoardingCompletedAt:
- *           type: string
- *           format: date-time
- *           nullable: true
- *           example: null
- *       required: [email, name]
- *
  *     User:
  *       type: object
  *       properties:
@@ -52,17 +19,17 @@
  *           enum: [Freelancer, Contractor]
  *           nullable: true
  *           example: null
- *         profileImage:
+ *         picture:
  *           type: string
- *           example: null
+ *           example: https://cdn.auth0.com/avatars/bi.png
  *           nullable: true
  *         province:
  *           type: string
- *           example: null
+ *           example: BC
  *           nullable: true
  *         currency:
  *           type: string
- *           example: null
+ *           example: CAD
  *           nullable: true
  *         onBoardingCompletedAt:
  *           type: string
--- backend/src/schemas/SignupSchema.js
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const SignupSchema = z.object({
-	sub: z.string().min(1),
-	"https://clario.com/email": z.email(),
-	"https://clario.com/name": z.string().min(3),
-	"https://clario.com/picture": z.url().optional(),
-});
