2ea77e0a137c05dcf01d992c4df0302bcc323e54
Daniel Velastegui <55715214+danielvelastegui@users.noreply.github.com>
2025-10-10 01:19:10 -0700

    wip: Transactions CRUD
    all services created, just Find controller created

---
+++ backend/src/services/index.js
@@ -0,0 +1,7 @@
+import * as AuthService from './auth/AuthService.js';
+import * as TransactionService from './transactions/TransactionsService.js';
+
+export {
+    AuthService,
+    TransactionService,
+};
