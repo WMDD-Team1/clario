6d3533c0a2967be2dcffc14321dc2e8e990ba04c
Yahui Xu <65892220+94yahui@users.noreply.github.com>
2025-10-22 16:38:34 -0700

    add switch tabs

--- frontend/src/pages/Clients.tsx
+++ frontend/src/pages/Clients.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from '@components/Button';
 import Input from '@components/Input';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -8,6 +8,8 @@ import InfoRow from '@components/InfoRow';
 import Slide from '@components/Slide';
 import Select from '@components/Select';
 import InsightCard from '@components/InsightCard';
+import ToggleButton from '@components/ToggleButton';
+import TabSwitcher from '@components/TabSwitcher';
 
 const Clients = () => {
   interface Project {
@@ -92,6 +94,8 @@ const Clients = () => {
 
   const { getAccessTokenSilently } = useAuth0();
 
+  const [activeTab, setActiveTab] = useState<string>('Unarchived');
+
   //--Get data from form--------------------------
   const fetchClients = async () => {
     const token = await getAccessTokenSilently({
@@ -102,14 +106,11 @@ const Clients = () => {
 
     // console.log(token);
 
-    const response = await axios.get(
-      `${import.meta.env.VITE_API_BASE_URL}/clients?page=${currentPage}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+    const response = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/clients?limit=1000`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
       },
-    );
+    });
     const data = await response.data;
     console.log(data);
     setClients(data);
@@ -137,33 +138,12 @@ const Clients = () => {
     }
   };
 
-  // const fetchOneClient = async () => {
-  //   const token = await getAccessTokenSilently({
-  //     authorizationParams: {
-  //       audience: import.meta.env.VITE_AUTH0_AUDIENCE as string,
-  //     },
-  //   });
-
-  //   // console.log(token);
-
-  //   const response = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/clients/${clientId}`, {
-  //     headers: {
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //   });
-  //   const data = await response.data;
-  //   console.log(data);
-  //   setOneClient(data);
-  // };
-
   const deleteClient = async () => {
     const token = await getAccessTokenSilently({
       authorizationParams: {
         audience: import.meta.env.VITE_AUTH0_AUDIENCE as string,
       },
     });
-
-    // console.log(token);
 
     const response = await axios.patch(
       `${import.meta.env.VITE_API_BASE_URL}/clients/${clientId}/archive`,
@@ -287,8 +267,22 @@ const Clients = () => {
     }
   };
 
+  const CLIENTS_PER_PAGE = 10;
+
   const filteredClients = clients.data
-    .filter((c) => !c.isArchived && c.name.toLowerCase().includes(searchText.toLowerCase()))
+    .filter((c) => {
+      const name = c.name.toLowerCase();
+      const search = searchText.toLowerCase();
+
+      if (activeTab === 'Unarchived') {
+        return !c.isArchived && name.includes(search);
+      } else if (activeTab === 'Archived') {
+        return c.isArchived && name.includes(search);
+      } else if (activeTab === 'All Clients') {
+        return name.includes(search);
+      }
+      return false;
+    })
     .sort((a, b) => {
       switch (select) {
         case 'Names(Newest-Oldest)':
@@ -303,20 +297,37 @@ const Clients = () => {
       }
     });
 
+  const pagedClients = filteredClients.slice(
+    (currentPage - 1) * CLIENTS_PER_PAGE,
+    currentPage * CLIENTS_PER_PAGE,
+  );
+
+  const totalPages = Math.ceil(filteredClients.length / CLIENTS_PER_PAGE);
+
+  const options = [
+    { key: 'projects', label: 'All Projects' },
+    { key: 'clients', label: 'All Clients' },
+  ];
+
+  const [selectedOption, setSelectedOption] = useState(options[1]);
+
+  const handleToggle = (option: { key: string; label: string }) => {
+    setSelectedOption(option);
+  };
+
   return (
     <div className="flex flex-col gap-[1rem] p-[1rem]">
-      <div className="flex flex-row justify-between sm:items-center items-start">
+      <div className="flex flex-row justify-between items-start ml:items-center">
         <div className="flex flex-row  flex-wrap justify-between items-center gap-[2rem]">
-          <h2 className="text-2xl">My Work</h2>
-          <div className="flex flex-row gap-[.5rem] justify-between items-center p-[.5rem] bg-gray-200 rounded-[1rem]">
-            <p className="rounded-[1rem] pt-[.5rem] pb-[.5rem] pl-[2rem] pr-[2rem]">All Projects</p>
-            <p className="bg-white rounded-[1rem] pt-[.5rem] pb-[.5rem] pl-[2rem] pr-[2rem]">
-              All Clients
-            </p>
-          </div>
+          <h2 className="text-2xl">
+            {selectedOption.key === 'projects' && 'My Work'}
+            {selectedOption.key === 'clients' && 'My Clients'}
+          </h2>
+          <ToggleButton options={options} option={selectedOption} onClick={handleToggle} />
         </div>
         <Button
           buttonColor="regularButton"
+          // need to switch to project page------------------------------
           onClick={() => {
             setSlide('0px');
             resetFormData();
@@ -324,7 +335,8 @@ const Clients = () => {
           textColor="white"
           width="200px"
         >
-          Add Client
+          {selectedOption.key === 'projects' && 'Add Project'}
+          {selectedOption.key === 'clients' && 'Add Clients'}
         </Button>
       </div>
       <div className="flex flex-row flex-wrap items-center gap-[1rem]">
@@ -336,136 +348,143 @@ const Clients = () => {
       </div>
 
       {/* client-list------------------------------------------------- */}
-      <div className="flex flex-row flex-nowrap items-center justify-start gap-[1rem]">
-        <div className="w-[20rem]">
-          <Input
-            placeholder="Search client"
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-          />
-        </div>
-        <Select
-          value={select}
-          color="bg-white"
-          onChange={setSelect}
-          placeHolder="Sort By"
-          options={['Names(Newest-Oldest)', 'Invoices(Fewest-Most)', 'Total Projects(Fewest-Most)']}
-        />
-      </div>
-
-      {filteredClients.length == 0 ? (
-        <div className="flex flex-col flex-nowrap items-center gap-[1rem] p-[3rem] border-2 border-dashed border-blue-100 bg-blue-50 rounded-[20px]">
-          <p className='text-gray-400 text-center'>No clients added yet. Start by adding your first client to begin managing projects.</p>
-          <Button
-            buttonColor="regularButton"
-            onClick={() => {
-              setSlide('0px');
-              resetFormData();
-            }}
-            textColor="white"
-            width="200px"
-          >
-            Add Client
-          </Button>
-        </div>
-      ) : (
-        <div className="rounded-[20px] border overflow-hidden border-gray-200">
-          <div className="overflow-x-auto w-full">
-          <table className="text-center w-full min-[720px]">
-            <thead className="bg-gray-200">
-              <tr>
-                <th className="text-left px-4 py-4">Client Name</th>
-                <th className="px-4 py-2">Phone</th>
-                <th className="px-4 py-2">Email</th>
-                <th className="px-4 py-2">Country</th>
-                <th className="px-4 py-2">Invoices</th>
-                <th className="px-4 py-2">Total Projects</th>
-                <th className="px-4 py-2"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredClients
-                .sort((a, b) => {
-                  switch (select) {
-                    case 'Names(Newest-Oldest)':
-                      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-
-                    case 'Invoices(Fewest->Most)':
-                      return a.invoiceCount - b.invoiceCount;
-                    case 'Total Projects(Fewest->Most)':
-                      return a.projectCount - b.projectCount;
-                    default:
-                      return 0;
-                  }
-                })
-                .map((client, index) => (
-                  <tr key={index} className="hover:bg-gray-100">
-                    <td className="text-left px-4 py-[1rem] border-t border-gray-200">
-                      {client.name}
-                    </td>
-                    <td className="px-4 border-t border-gray-200">{client.phone}</td>
-                    <td className="px-4 border-t border-gray-200">{client.email}</td>
-                    <td className="px-4 border-t border-gray-200">{client.address.country}</td>
-                    <td className="px-4 border-t border-gray-200">{client.invoiceCount}</td>
-                    <td className="px-4 border-t border-gray-200">{client.projectCount}</td>
-                    <td
-                      className="px-4 border-t border-gray-200"
-                      onClick={() => handleClientDetail(client.id)}
-                    >
-                      ...
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+      {selectedOption.key === 'clients' && (
+        <>
+          <div className="flex flex-row flex-wrap items-center justify-start gap-[1rem]">
+            <TabSwitcher
+              tabs={['All Clients', 'Unarchived', 'Archived']}
+              activeTab={activeTab}
+              onChange={setActiveTab}
+            />
+            <div className="w-[20rem]">
+              <Input
+                placeholder="Search client"
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+              />
+            </div>
+            <Select
+              width="150px"
+              value={select}
+              color="bg-white"
+              onChange={setSelect}
+              placeHolder="Sort By"
+              options={[
+                'Names(Newest-Oldest)',
+                'Invoices(Fewest-Most)',
+                'Total Projects(Fewest-Most)',
+              ]}
+            />
           </div>
-          <div className="bg-gray-200 w-full p-[1rem] flex felx-row flex-nowrap justify-between items-center">
-            <p>Total {clients.meta.total}</p>
-            <div className="flex felx-row flex-nowrap justify-between items-center gap-[1rem]">
-              <div
-                onClick={() => {
-                  if (currentPage > 1) {
-                    setCurrentPage(currentPage - 1);
-                    if (currentPage - 1 < pageWindowStart) {
-                      setPageWindowStart(pageWindowStart - PAGE_WINDOW_SIZE);
-                    }
-                  }
-                }}
-              >
-                {'<'}
-              </div>
-              {Array.from({
-                length: Math.min(PAGE_WINDOW_SIZE, clients.meta.totalPages - pageWindowStart + 1),
-              }).map((_, i) => {
-                const pageNumber = pageWindowStart + i;
-                return (
-                  <div
-                    key={pageNumber}
-                    onClick={() => setCurrentPage(pageNumber)}
-                    className={`flex justify-center items-center p-[.5rem] border border-gray-400 rounded-[5px] h-[25px] w-[25px] hover:bg-gray-300 ${currentPage === pageNumber ? 'bg-gray-400' : 'bg-gray-200'}`}
-                  >
-                    {pageNumber}
-                  </div>
-                );
-              })}
 
-              <div
+          {filteredClients.length == 0 ? (
+            <div className="flex flex-col flex-nowrap items-center gap-[1rem] p-[3rem] border-2 border-dashed border-blue-100 bg-blue-50 rounded-[20px]">
+              <p className="text-gray-400 text-center">
+                No clients added yet. Start by adding your first client to begin managing projects.
+              </p>
+              <Button
+                buttonColor="regularButton"
                 onClick={() => {
-                  if (currentPage < clients.meta.totalPages) {
-                    setCurrentPage(currentPage + 1);
-                    if (currentPage + 1 >= pageWindowStart + PAGE_WINDOW_SIZE) {
-                      setPageWindowStart(pageWindowStart + PAGE_WINDOW_SIZE);
-                    }
-                  }
+                  setSlide('0px');
+                  resetFormData();
                 }}
+                textColor="white"
+                width="200px"
               >
-                {'>'}
+                Add Client
+              </Button>
+            </div>
+          ) : (
+            <div className="rounded-[20px] border overflow-hidden border-gray-200">
+              <div className="overflow-x-auto w-full">
+                <table className="text-center w-full min-[720px]">
+                  <thead className="bg-gray-200">
+                    <tr>
+                      <th className="text-left px-4 py-4">Client Name</th>
+                      <th className="px-4 py-2">Phone</th>
+                      <th className="px-4 py-2">Email</th>
+                      <th className="px-4 py-2">Country</th>
+                      <th className="px-4 py-2">Invoices</th>
+                      <th className="px-4 py-2">Total Projects</th>
+                      <th className="px-4 py-2"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {pagedClients.map((client, index) => (
+                        <tr key={index} className="hover:bg-gray-100">
+                          <td className="text-left px-4 py-[1rem] border-t border-gray-200">
+                            {client.name}
+                          </td>
+                          <td className="px-4 border-t border-gray-200">{client.phone}</td>
+                          <td className="px-4 border-t border-gray-200">{client.email}</td>
+                          <td className="px-4 border-t border-gray-200">
+                            {client.address.country}
+                          </td>
+                          <td className="px-4 border-t border-gray-200">{client.invoiceCount}</td>
+                          <td className="px-4 border-t border-gray-200">{client.projectCount}</td>
+                          <td
+                            className="px-4 border-t border-gray-200"
+                            onClick={() => handleClientDetail(client.id)}
+                          >
+                            ...
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
+              <div className="bg-gray-200 w-full p-[1rem] flex felx-row flex-nowrap justify-between items-center">
+                <p>Total {filteredClients.length}</p>
+                <div className="flex felx-row flex-nowrap justify-between items-center gap-[1rem]">
+                  <div
+                    onClick={() => {
+                      if (currentPage > 1) {
+                        setCurrentPage(currentPage - 1);
+                        if (currentPage - 1 < pageWindowStart) {
+                          setPageWindowStart(pageWindowStart - PAGE_WINDOW_SIZE);
+                        }
+                      }
+                    }}
+                  >
+                    {'<'}
+                  </div>
+                  {Array.from({
+                    length: Math.min(
+                      PAGE_WINDOW_SIZE,
+                      totalPages - pageWindowStart + 1,
+                    ),
+                  }).map((_, i) => {
+                    const pageNumber = pageWindowStart + i;
+                    return (
+                      <div
+                        key={pageNumber}
+                        onClick={() => setCurrentPage(pageNumber)}
+                        className={`flex justify-center items-center p-[.5rem] border border-gray-400 rounded-[5px] h-[25px] w-[25px] hover:bg-gray-300 ${currentPage === pageNumber ? 'bg-gray-400' : 'bg-gray-200'}`}
+                      >
+                        {pageNumber}
+                      </div>
+                    );
+                  })}
+
+                  <div
+                    onClick={() => {
+                      if (currentPage < totalPages) {
+                        setCurrentPage(currentPage + 1);
+                        if (currentPage + 1 >= pageWindowStart + PAGE_WINDOW_SIZE) {
+                          setPageWindowStart(pageWindowStart + PAGE_WINDOW_SIZE);
+                        }
+                      }
+                    }}
+                  >
+                    {'>'}
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          )}
+        </>
       )}
-
+      {/* Add-Client----------------------------------- */}
       <Slide
         title="Add Client"
         confirmText="Add"
@@ -559,7 +578,7 @@ const Clients = () => {
         <InfoRow label="Client Name" value={oneClient.name} />
         <InfoRow label="Phone" value={oneClient.phone} />
         <InfoRow label="Email" value={oneClient.email} />
-        <InfoRow label="Notes" value={oneClient.notes} vertical = {true}/>
+        <InfoRow label="Notes" value={oneClient.notes} vertical={true} />
         <InfoRow label="Street Address" value={oneClient.address?.street} />
         <InfoRow label="Postal Code" value={oneClient.address?.postalCode} />
         <InfoRow label="City" value={oneClient.address?.city} />
@@ -657,6 +676,9 @@ const Clients = () => {
           }
         />
       </Slide>
+
+      {/* Projects------------- */}
+      {selectedOption.key === 'projects' && <></>}
     </div>
   );
 };
