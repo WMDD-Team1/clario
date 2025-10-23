341443348c225a8d213ce84ca9e61357c11367dd
Daniel Velastegui <55715214+danielvelastegui@users.noreply.github.com>
2025-10-22 19:47:18 -0700

    refactor: projects components and responsive

--- frontend/src/pages/MyWork.tsx
+++ frontend/src/pages/MyWork.tsx
@@ -20,20 +20,37 @@ const MyWork = () => {
     });
     return (
         <>
-            <div className="header flex justify-between items-center mb-8">
-                <div className="header-left flex items-center gap-4">
-                    <h1 className='text-[32px] font-serif'>My Work</h1>
+            <div className='header mb-8'>
+                <section className=" md:flex justify-between items-center hidden transition">
+                    <div className="header-left flex items-center gap-4">
+                        <h2 className='text-[32px] font-serif'>My Work</h2>
+                        <ToggleButton options={views} option={view} onClick={setView} />
+                    </div>
+                    <Button
+                        buttonColor="regularButton"
+                        onClick={() => console.log()}
+                        textColor="white"
+                        width="200px"
+                    >
+                        Add {view.key === 'projects' ? "Project" : "Client"}
+                    </Button>
+                </section>
+                <section className='md:hidden transition'>
+                    <div className="flex items-center justify-between mb-4">
+                        <h2 className="text-2xl font-semibold text-black">My Work</h2>
+                        <Button
+                            buttonColor="regularButton"
+                            onClick={() => console.log()}
+                            textColor="white"
+                            width="200px"
+                        >
+                            Add {view.key === 'projects' ? "Project" : "Client"}
+                        </Button>
+                    </div>
                     <ToggleButton options={views} option={view} onClick={setView} />
-                </div>
-                <Button
-                    buttonColor="regularButton"
-                    onClick={() => console.log()}
-                    textColor="white"
-                    width="200px"
-                >
-                    Add {view.key === 'projects' ? "Project" : "Client"}
-                </Button>
+                </section>
             </div>
+
             <div>{view.key === 'projects' ? <Projects /> : null}</div>
         </>
     )
