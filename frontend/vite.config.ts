75c356da5c48258f58b835e89332770d6fde7654
Bitna / Erin <64954355+bitna-dev@users.noreply.github.com>
2025-10-23 17:49:24 -0700

    chore: add absolute path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@api': path.resolve(__dirname, 'src/api'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-})
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+});
