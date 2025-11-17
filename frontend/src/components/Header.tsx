c1aefaec937c27d484980c60f14132878d97ed89
Bitna / Erin <64954355+bitna-dev@users.noreply.github.com>
2025-11-17 11:03:51 -0800

    Revert "fix: fix last PR error"
    This reverts commit 1cc8359745035d7d697dedf69c57667b5a823622.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,9 +7,9 @@ import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 import { ProjectApiResponse } from '@api/types/projectApi';
 import { ClientApiResponse } from '@api/types/clientApi';
-
+import { Link } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
-import { fetchAllClients, fetchAllProjects } from '@/api';
+import { fetchAllClients,fetchAllProjects } from '@/api';
 
 type SearchResultType = 'project' | 'client';
 
@@ -79,14 +79,11 @@ const Header = () => {
     .filter((item) => {
       const name = item.name?.toLowerCase();
       return name.includes(searchValue.toLowerCase());
-    })
-    .sort((a, b) => a.name.localeCompare(b.name));
+    }).sort((a,b)=>a.name.localeCompare(b.name));
 
   return (
-    <div
-      className="flex m-0 py-[10px] px-[20px] md:p-[40px] justify-between items-center sticky top-0 bg-[var(--background)] mb-5 z-[1000]
-    md:bg-[var(--background)] rounded-b-[20px] shadow-md md:rounded-none md:shadow-none"
-    >
+    <div className="flex m-0 py-[10px] px-[20px] md:p-[40px] justify-between items-center sticky top-0 bg-[var(--background)] mb-5 z-[1000]
+    md:bg-[var(--background)] rounded-b-[20px] shadow-md md:rounded-none md:shadow-none">
       <div
         className={`logo transition-all duration-3000 ${
           isSearchOpen ? 'hidden md:block' : 'block'
@@ -97,53 +94,53 @@ const Header = () => {
       </div>
 
       <div ref={searchContainerRef}>
-        <div className="flex items-center justify-between gap-[20px]">
-          {/* <div className="flex flex-col items-center"> */}
-          <SearchBar
-            isSearchOpen={isSearchOpen}
-            onSearchOpen={setIsSearchOpen}
-            searchValue={searchValue}
-            onChange={setSearchValue}
+      <div className="flex items-center justify-between gap-[20px]">
+        {/* <div className="flex flex-col items-center"> */}
+        <SearchBar
+          isSearchOpen={isSearchOpen}
+          onSearchOpen={setIsSearchOpen}
+          searchValue={searchValue}
+          onChange={setSearchValue}
+        >
+          <div
+            className={`absolute bg-[var(--primitive-colors-brand-primary-025)] border border-[var(--primitive-colors-gray-light-mode-200)] shadow-md backdrop-blur-sm p-[1rem] rounded-xl top-[.5rem] w-full ${searchValue ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-5'} transition-all duration-300 max-h-[200px] overflow-y-scroll`}
           >
             <div
-              className={`absolute bg-[var(--primitive-colors-brand-primary-025)] border border-[var(--primitive-colors-gray-light-mode-200)] shadow-md backdrop-blur-sm p-[1rem] rounded-xl top-[.5rem] w-full ${searchValue ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-5'} transition-all duration-300 max-h-[200px] overflow-y-scroll`}
+              className={`absolute bg-[var(--primitive-colors-brand-primary-025)] border border-[var(--primitive-colors-gray-light-mode-200)] shadow-md backdrop-blur-sm p-[1rem] rounded-xl top-[.5rem] w-full ${
+                searchValue ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-5'
+              } transition-all duration-300`}
             >
-              <div
-                className={`absolute bg-[var(--primitive-colors-brand-primary-025)] border border-[var(--primitive-colors-gray-light-mode-200)] shadow-md backdrop-blur-sm p-[1rem] rounded-xl top-[.5rem] w-full ${
-                  searchValue ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-5'
-                } transition-all duration-300`}
-              >
-                {filteredResults.length !== 0 && searchValue ? (
-                  <>
-                    {filteredResults.map((project, index) => (
-                      <Link
-                        onClick={() => {
-                          setSearchValue('');
-                          setIsSearchOpen(false);
-                        }}
-                        to={
-                          project.type === 'project'
-                            ? `/projects/${project.id}`
-                            : `/projects?clientId=${project.id}`
-                        }
-                        key={index}
-                        className="flex flex-row justify-between items-center mb-1 pb-0.5 cursor-pointer border-b-0 transition-all relative group"
-                      >
-                        {project.name}
-                        <p className="text-[.8rem]">{project.type}</p>
-                        <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-[var(--primitive-colors-brand-primary-400)] transition-all duration-300 ease-out group-hover:left-0 group-hover:w-full" />
-                      </Link>
-                    ))}
-                  </>
-                ) : (
-                  <>{searchValue ? 'Not found' : ''}</>
-                )}
-              </div>
+              {filteredResults.length !== 0 && searchValue ? (
+                <>
+                  {filteredResults.map((project, index) => (
+                    <Link
+                      onClick={() => {
+                        setSearchValue('');
+                        setIsSearchOpen(false);
+                      }}
+                      to={
+                        project.type === 'project'
+                          ? `/projects/${project.id}`
+                          : `/projects?clientId=${project.id}`
+                      }
+                      key={index}
+                      className="flex flex-row justify-between items-center mb-1 pb-0.5 cursor-pointer border-b-0 transition-all relative group"
+                    >
+                      {project.name}
+                      <p className="text-[.8rem]">{project.type}</p>
+                      <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-[var(--primitive-colors-brand-primary-400)] transition-all duration-300 ease-out group-hover:left-0 group-hover:w-full" />
+                    </Link>
+                  ))}
+                </>
+              ) : (
+                <>{searchValue ? 'Not found' : ''}</>
+              )}
             </div>
           </SearchBar>
 
           {/* ---------------- USER ICON SECTION ---------------- */}
           <div className="relative">
+
             {/* DESKTOP USER IMAGE */}
             <div
               className={`hidden md:block transition-all duration-3000 ${
@@ -183,7 +180,7 @@ const Header = () => {
                   hover:[var(--primitive-colors-gray-light-mode-100)] active:[var(--primitive-colors-gray-light-mode-200)] active:scale-[0.98]"
                   onClick={() => setMobileMenuOpen(false)}
                 >
-                  FAQ
+                   FAQ
                 </Link>
 
                 <Link
@@ -192,11 +189,12 @@ const Header = () => {
                   hover:[var(--primitive-colors-gray-light-mode-100)] active:[var(--primitive-colors-gray-light-mode-200)] active:scale-[0.98]"
                   onClick={() => setMobileMenuOpen(false)}
                 >
-                  Logout
+                   Logout
                 </Link>
               </div>
             )}
           </div>
+
         </div>
       </div>
     </div>
