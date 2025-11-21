23621e6d402df47a22ab8c993e3bded5ffcc8259
Daniel Velastegui <55715214+danielvelastegui@users.noreply.github.com>
2025-11-21 12:11:36 -0800

    fix(navbar): color and padding

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -15,7 +15,7 @@ interface Props {
 const Menu = ({ items }: Props) => {
 
     return (
-        <div className='flex flex-col items-center justify-between py-[30px] px-5 bg-white  rounded-[30px] shadow-[0_85px_24px_0_rgba(0,0,0,0),_0_54px_22px_0_rgba(0,0,0,0.01),_0_31px_18px_0_rgba(0,0,0,0.02),_0_14px_14px_0_rgba(0,0,0,0.04),_0_3px_7px_0_rgba(0,0,0,0.04)]'>
+        <div className='flex flex-col items-center justify-between py-[30px] px-5 bg-white  rounded-[30px] shadow-[0_20px_50px_-30px_#afafaf75]'>
             <nav className='flex flex-col gap-10'>
                 {items.map(({ tooltip, route, activeIcon: ActiveIcon, inactiveIcon: InactiveIcon }) => (
                     <Tooltip key={route} title={tooltip} arrow placement="right" slotProps={{
