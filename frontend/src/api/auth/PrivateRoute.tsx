700c12c7a3320623c02d0b26b650e5f3ace223b6
Bitna / Erin <64954355+bitna-dev@users.noreply.github.com>
2025-11-30 21:36:31 -0800

    fix: redefine userslice

--- frontend/src/api/auth/PrivateRoute.tsx
+++ frontend/src/api/auth/PrivateRoute.tsx
@@ -7,7 +7,7 @@ import Sidebar from '@components/Sidebar';
 import { fetchUser } from '@store/userSlice';
 import { ReactNode, useEffect } from 'react';
 import { useMediaQuery } from 'react-responsive';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 export const PrivateRoute = ({ children }: { children: ReactNode }) => {
   const dispatch = useAppDispatch();
@@ -17,16 +17,27 @@ export const PrivateRoute = ({ children }: { children: ReactNode }) => {
     dispatch(fetchUser());
   }, []);
 
+  const loc = useLocation();
+  const pathname = loc.pathname;
+  console.log(pathname);
+
   const isDesktop = useMediaQuery({ minWidth: 768 });
 
-  if (loading || user == null) return <Loading />;
+  if (loading || user == undefined) return <Loading />;
+
+  if (user == null) return <Navigate to="/login" replace />;
 
   if (error) return <Error message={error} />;
 
   return (
     <>
-      <Header />
-      {isDesktop ? <Sidebar /> : <Navbar />}
+      {pathname !== '/onboarding' && (
+        <>
+          <Header />
+          {isDesktop ? <Sidebar /> : <Navbar />}
+        </>
+      )}
+
       <div className="grow-1 md:ml-[170px] mx-[20px] mb-[160px] md:mb-[40px]">{children}</div>
     </>
   );
